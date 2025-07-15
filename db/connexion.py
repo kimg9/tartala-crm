aa3cd16f436a6c140d7527bd9b0129c79dc392f9
gemo <kim.gemo@gmail.com>
2025-07-15 13:43:29 +0200

    simplify db connexion utils

--- db/connexion.py
+++ db/connexion.py
@@ -1,16 +1,11 @@
 from sqlalchemy import URL, create_engine
-from sqlalchemy.orm import sessionmaker
 
-from models import *
+from models.models import *
 
 from .base import Base
-
 
 url_object = URL.create("sqlite", database="tartala-crm")
 
 engine = create_engine(url_object, echo=True)
-
-Session = sessionmaker(bind=engine)
-session = Session()
 
 Base.metadata.create_all(engine)
